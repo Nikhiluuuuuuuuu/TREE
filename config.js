29426ed9e76a184edcf1bd2230df06d504a294be
Nikhil Sai Pagidimarri <nikhilsai384@gmail.com>
2026-05-21 12:39:29 +0530

    Create config.js with dynamic user configuration

---
+++ config.js
@@ -0,0 +1,11 @@
+// User Configuration
+// Edit this object to change author information displayed in the app
+
+const USER_CONFIG = {
+    username: 'Nikhiluuuuuuuuu',
+    displayName: 'Nikhil',
+    profileUrl: 'https://github.com/Nikhiluuuuuuuuu',
+    profileImageUrl: 'https://github.com/Nikhiluuuuuuuuu.png',
+    repoName: 'TREE',
+    repositoryUrl: 'https://github.com/Nikhiluuuuuuuuu/TREE'
+};
